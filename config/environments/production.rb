5e76828c7af3cf4625e3339f9dd490bfd9f2fcb3
abeshumpei <s_abe@gacci.jp>
2023-12-07 17:23:11 +0900

    本番許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,5 +102,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << "rails-react-app-backend.onrender.com"
+  # 全てのホストを許可する
+  config.hosts.clear
 end
